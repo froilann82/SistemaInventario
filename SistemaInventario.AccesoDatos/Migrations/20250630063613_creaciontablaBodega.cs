b406969c8b33c20fc828ece61f7e89ca05c6b734
froilan82 <froilann82@gmail.com>
2025-06-30 02:03:14 -0500

    Configuracion Proyecto

---
+++ SistemaInventario.AccesoDatos/Migrations/20250630063613_creaciontablaBodega.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaInventario.AccesoDatos.Migrations
+{
+    public partial class creaciontablaBodega : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ SistemaInventario.AccesoDatos/Migrations/20250630065701_actualizaTablaBodega.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaInventario.AccesoDatos.Migrations
+{
+    public partial class actualizaTablaBodega : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
